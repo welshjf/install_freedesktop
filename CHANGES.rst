eeb89be5f8bea4c98399267149f70b5dfa9f86cd
Jacob Welsh <jacob@welshcomputing.com>
2015-01-26 12:18:39 -0500

    Concatenate README/CHANGES for long_description

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,10 @@
-=======
 Changes
 =======
 
-Version 0.1.0
-=============
+* Use README/CHANGES in long_description for PyPI
+* Fix syntax in README examples
+
+0.1.0 (2015-01-26)
+------------------
 
 * Initial release
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
+import codecs
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    README = f.read()
+with codecs.open('CHANGES.rst', encoding='utf-8') as f:
+    CHANGES = f.read()
 
 setup(
     name='install_freedesktop',
     version='0.1.0',
     description='Setuptools extension to install freedesktop.org app icons',
-    long_description="""\
-Setuptools extension to install launcher icons for KDE, GNOME, or other
-freedesktop.org compatible Linux/UNIX environments.""",
+    long_description=README + '\n\n' + CHANGES,
     author='Jacob Welsh',
     author_email='jacob@welshcomputing.com',
     url='https://github.com/welshjf/install_freedesktop',
